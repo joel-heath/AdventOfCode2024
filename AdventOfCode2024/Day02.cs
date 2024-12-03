4948e16d6bb55ea4dc029458ea18b29d599e86e7
Joel Heath <joelheath@outlook.com>
2024-12-03 05:27:26 +0000

    Day 3 initial solution

--- AdventOfCode2024/Day02.cs
+++ AdventOfCode2024/Day02.cs
@@ -12,20 +12,20 @@ public class Day02 : IDay
         { "7 6 4 2 1\r\n1 2 7 8 9\r\n9 7 6 2 1\r\n1 3 2 4 5\r\n8 6 4 4 1\r\n1 3 6 7 9", "4" }
     };
 
-    static bool IsValid(int[] line)
-        => line.Skip(1).Aggregate(
-                (prev: line[0], dir: line[1] > line[0] ? 1 : -1, valid: true),
+    static bool IsValid(int[] report)
+        => report.Skip(1).Aggregate(
+                (prev: report[0], dir: report[1] > report[0] ? 1 : -1, valid: true),
                 (acc, curr) => (curr, acc.dir, acc.valid && acc.dir * (curr - acc.prev) >= 1 && acc.dir * (curr - acc.prev) <= 3)
             ).valid;
 
     public string SolvePart1(string input)
         => $"{input.Split(Environment.NewLine)
             .Select(l => l.Split(' ').Select(int.Parse))
-            .Count(l => IsValid([..l]))}";
+            .Count(r => IsValid([..r]))}";
 
     public string SolvePart2(string input)
         => $"{input.Split(Environment.NewLine)
             .Select(l => (int[])[..l.Split(' ').Select(int.Parse)])
-            .Select(l => Enumerable.Range(0, l.Length).Select(n => (int[])[..l[..n], ..l[(n + 1)..]]))
-            .Count(o => o.Any(IsValid))}";
+            .Select(r => Enumerable.Range(0, r.Length).Select(n => (int[])[..r[..n], ..r[(n + 1)..]]))
+            .Count(r => r.Any(IsValid))}";
 }
--- AdventOfCode2024/Day03.cs
+++ AdventOfCode2024/Day03.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode2024;
 
 public class Day03 : IDay
@@ -5,22 +7,24 @@ public class Day03 : IDay
     public int Day => 3;
     public Dictionary<string, string> UnitTestsP1 => new()
     {
-        { "TestInput1", "ExpectedOutput1" },
-        { "TestInput2", "ExpectedOutput2" }
+        { "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))", "161" }
     };
     public Dictionary<string, string> UnitTestsP2 => new()
     {
-        { "TestInput1", "ExpectedOutput1" },
-        { "TestInput2", "ExpectedOutput2" }
+        { "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))", "48" }
     };
 
     public string SolvePart1(string input)
     {
-        return string.Empty;
+        var matches = Regex.Matches(input, @"mul\((\d+),(\d+)\)").Select(m => m.Groups.Cast<Group>().Select(g => g.Captures.Select(c => int.Parse(c.Value))).Skip(1).SelectMany(e => e).ToArray());
+        return $"{matches.Sum(m => m[0] * m[1])}";
     }
 
     public string SolvePart2(string input)
     {
-        return string.Empty;
+        var muls = Regex.Matches(input, @"mul\((\d+),(\d+)\)").Select(m => m.Groups.Cast<Group>().Select(g => g.Captures.Select(c => (n: int.Parse(c.Value), i: c.Index))).Skip(1).SelectMany(e => e).ToArray());
+        var dos = Regex.Matches(input, @"do(n't)?\(\)").Select(m => (b: m.Value == "do()", i: m.Index)).Reverse().ToArray();
+
+        return $"{muls.Where(m => dos.FirstOrDefault(d => d.i < m[0].i, (b:true, i:-1)).b).Sum(m => m[0].n * m[1].n)}";
     }
 }
