4aa2bc53ca312362be60313b101addea5cb78b4a
Joel Heath <joelheath@outlook.com>
2024-12-05 06:29:36 +0000

    Day 5 reduced

--- AdventOfCode2024/Day05.cs
+++ AdventOfCode2024/Day05.cs
@@ -14,76 +14,32 @@ public class Day05 : IDay
 
     public string SolvePart1(string input)
     {
-        long sum = 0;
-
         var sections = input.Split(Environment.NewLine + Environment.NewLine);
-        var orders = sections[0].Split(Environment.NewLine).Select(l => l.Split('|').Select(long.Parse).ToArray()).GroupBy(x => x[0]).ToDictionary(g => g.Key, g => g.Select(a => a[1]).ToArray());
-
-        var lines = sections[1].Split(Environment.NewLine);
-        for (int i = 0; i < lines.Length; i++)
-        {
-            var line = lines[i].Split(',').Select(long.Parse).ToArray();
-
-            bool correctOrder = true;
-            for (int j = 0; j < line.Length; j++)
-            {
-                var num = line[j];
-                if (!orders.TryGetValue(num, out long[]? proceedings))
-                    proceedings = [];
-                if (line[..j].Any(n => proceedings.Contains(n)))
-                {
-                    correctOrder = false;
-                    break;
-                }
-            }
-
-            if (correctOrder)
-                sum += line[line.Length / 2];
-        }
-
-        return $"{sum}";
+        var orders = sections[0].Split(Environment.NewLine).Select(l => l.Split('|').Select(int.Parse).ToArray()).GroupBy(l => l[0]).ToDictionary(g => g.Key, g => g.Select(l => l[1]).ToHashSet());
+        return $"{sections[1].Split(Environment.NewLine).Select(l => l.Split(',').Select(int.Parse).ToArray())
+            .Sum(l => l.Select(
+                    (curr, i) => l[..i].Any(
+                        pre => (orders.TryGetValue(curr, out var successors) ? successors : []).Contains(pre)
+                    )
+                ).Any(b => b) ? 0 : l[l.Length / 2]
+            )}";
     }
 
     public string SolvePart2(string input)
     {
-        long sum = 0;
-
         var sections = input.Split(Environment.NewLine + Environment.NewLine);
-        var orders = sections[0].Split(Environment.NewLine).Select(l => l.Split('|').Select(long.Parse).ToArray()).GroupBy(x => x[0]).ToDictionary(g => g.Key, g => g.Select(a => a[1]).ToArray());
-
-        var lines = sections[1].Split(Environment.NewLine).Select(l => l.Split(',').Select(long.Parse).ToArray()).ToArray();
-        HashSet<int> incorrectlyOrdered = [];
-        for (int i = 0; i < lines.Length; i++)
-        {
-            var line = lines[i];
-
-            bool correctOrder = true;
-            for (int j = 0; j < line.Length; j++)
-            {
-                var num = line[j];
-                if (!orders.TryGetValue(num, out long[]? proceedings))
-                    proceedings = [];
-
-                for (int k = 0; k < j; k++)
-                {
-                    if (proceedings.Contains(line[k]))
-                    {
-                        correctOrder = false;
-                        (lines[i][j], lines[i][k]) = (line[k], line[j]);
-                        incorrectlyOrdered.Add(i);
-                        i--; // Retry this line
-                        break;
-                    }
-                }
-                if (!correctOrder)
-                    break;
-            }
-            
-            // we've now corrected the order
-            if (correctOrder && incorrectlyOrdered.Contains(i))
-                sum += lines[i][line.Length / 2];
-        }
-
-        return $"{sum}";
+        var orders = sections[0].Split(Environment.NewLine).Select(l => l.Split('|').Select(long.Parse).ToArray()).GroupBy(l => l[0]).ToDictionary(g => g.Key, g => g.Select(l => l[1]).ToHashSet());
+        return $"{sections[1].Split(Environment.NewLine).Select(l => l.Split(',').Select(long.Parse).ToArray()).ToArray()
+            .Select((l, i) => Utils.EnumerateForever()
+                .AggregateWhile((correctOrder: true, firstAttempt: true), (acc, _) => (acc.correctOrder = ValidateOrder(orders, l), acc.firstAttempt &= acc.correctOrder), acc => !acc.correctOrder)
+                .firstAttempt ? 0 : l[l.Length / 2]
+            ).Sum()}";
     }
+
+    private static bool ValidateOrder(Dictionary<long, HashSet<long>> orders, long[] line)
+        => !line.Select(
+                (curr, i) => line[..i].Select(
+                    (pre, j) => (orders.TryGetValue(curr, out var successors) ? successors : []).Contains(pre) ? (line[i], line[j]) = (line[j], line[i]) : (-1, -1)
+                ).Any(i => i != (-1, -1))
+            ).Any(b => b);
 }
