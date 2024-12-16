a72a06b90c11435d2f8b76fad2af7f5ea375a2ce
Joel Heath <joelheath@outlook.com>
2024-12-16 07:09:16 +0000

    Day 16 README. I don't think I'm optimising many of these from here on out.

--- AdventOfCode2024/Day16.cs
+++ AdventOfCode2024/Day16.cs
@@ -16,19 +16,27 @@ public class Day16 : IDay
         { "###############\r\n#.......#....E#\r\n#.#.###.#.###.#\r\n#.....#.#...#.#\r\n#.###.#####.#.#\r\n#.#.#.......#.#\r\n#.#.#####.###.#\r\n#...........#.#\r\n###.#.#####.#.#\r\n#...#.....#.#.#\r\n#.#.#.###.#.#.#\r\n#.....#...#.#.#\r\n#.###.#.#.#.#.#\r\n#S..#.....#...#\r\n###############", "45" },
         { "#################\r\n#...#...#...#..E#\r\n#.#.#.#.#.#.#.#.#\r\n#.#.#.#...#...#.#\r\n#.#.#.#.###.#.#.#\r\n#...#.#.#.....#.#\r\n#.#.#.#.#.#####.#\r\n#.#...#.#.#.....#\r\n#.#.#####.#.###.#\r\n#.#.#.......#...#\r\n#.#.###.#####.###\r\n#.#.#...#.....#.#\r\n#.#.#.#####.###.#\r\n#.#.#.........#.#\r\n#.#.#.#########.#\r\n#S#.............#\r\n#################", "64" }
     };
-
     
     private static readonly Point[] dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)];
 
-    private static int SolveMaze(Grid<char> map, Point start, Point end)
+    private static IEnumerable<(Point p, Point dir)> Adjacents(Point p, Point dir)
     {
-        var queue = new PriorityQueue<(Point pos, Point dir, int steps), int>();
-        queue.Enqueue((start, (1, 0), 0), 0);
+        yield return (p + dir, dir);
+        var index = Array.IndexOf(dirs, dir);
+        var right = dirs[Utils.Mod(index + 1, 4)];
+        yield return (p + right, right);
+        yield return (p - right, -right);
+    }
+
+    private static int SingleSolve(Grid<char> map, Point start, Point end)
+    {
+        var queue = new PriorityQueue<(Point pos, Point dir), int>();
+        queue.Enqueue((start, (1, 0)), 0);
         int bestSoFar = int.MaxValue;
         HashSet <(Point pos, Point dir)> visited = [];
-        while (queue.TryDequeue(out var data, out int _))
+        while (queue.TryDequeue(out var data, out int steps))
         {
-            var (current, dir, steps) = data;
+            var (current, dir) = data;
             if (steps >= bestSoFar)
                 continue;
             if (current == end)
@@ -44,22 +52,22 @@ public class Day16 : IDay
             foreach (var (next, newDir) in Adjacents(current, dir).Where(n => map[n.p] != '#'))
             {
                 var cost = dir == newDir ? steps + 1 : steps + 1000 + 1;
-                queue.Enqueue((next, newDir, cost), cost);
+                queue.Enqueue((next, newDir), cost);
             }
         }
         return bestSoFar;
     }
 
-    private static int SolveMaze2(Grid<char> map, Point start, Point end)
+    private static int SolveAll(Grid<char> map, Point start, Point end)
     {
-        var queue = new PriorityQueue<(Point pos, Point dir, int steps, HashSet<Point> history), int>();
-        queue.Enqueue((start, (1, 0), 0, []), 0);
+        var queue = new PriorityQueue<(Point pos, Point dir, HashSet<Point> history), int>();
+        queue.Enqueue((start, (1, 0), []), 0);
         int bestSoFar = int.MaxValue;
         Dictionary<(Point pos, Point dir), int> visited = [];
         HashSet<Point> bestPath = [];
-        while (queue.TryDequeue(out var data, out int _))
+        while (queue.TryDequeue(out var data, out int steps))
         {
-            var (current, dir, steps, history) = data;
+            var (current, dir, history) = data;
             if (steps > bestSoFar)
                 continue;
             if (current == end)
@@ -68,7 +76,7 @@ public class Day16 : IDay
                 {
                     bestPath.UnionWith(history);
                 }
-                else // if (steps < bestSoFar)
+                else
                 {
                     bestPath = [..history];
                     bestSoFar = steps;
@@ -85,24 +93,15 @@ public class Day16 : IDay
             history = [.. history];
             history.Add(current);
 
-            foreach (var (next, newDir) in Adjacents(current, dir).Where(n => map[n.p] != '#'))
+            foreach (var (next, newDir) in Adjacents(current, dir).Where(n => !history.Contains(n.p) && map[n.p] != '#'))
             {
                 var cost = dir == newDir ? steps + 1 : steps + 1000 + 1;
-                queue.Enqueue((next, newDir, cost, history), cost);
+                queue.Enqueue((next, newDir, history), cost);
             }
         }
 
         bestPath.Add(end);
         return bestPath.Count;
-    }
-
-    private static IEnumerable<(Point p, Point dir)> Adjacents(Point p, Point dir)
-    {
-        yield return (p + dir, dir);
-        var index = Array.IndexOf(dirs, dir);
-        var right = dirs[Utils.Mod(index + 1, 4)];
-        yield return (p + right, right);
-        yield return (p - right, -right);
     }
 
     public string SolvePart1(string input)
@@ -111,7 +110,7 @@ public class Day16 : IDay
         var start = map.AllPositions().First(p => map[p] == 'S');
         var end = map.AllPositions().First(p => map[p] == 'E');
 
-        return $"{SolveMaze(map, start, end)}";
+        return $"{SingleSolve(map, start, end)}";
     }
 
     public string SolvePart2(string input)
@@ -120,6 +119,6 @@ public class Day16 : IDay
         var start = map.AllPositions().First(p => map[p] == 'S');
         var end = map.AllPositions().First(p => map[p] == 'E');
 
-        return $"{SolveMaze2(map, start, end)}";
+        return $"{SolveAll(map, start, end)}";
     }
 }
