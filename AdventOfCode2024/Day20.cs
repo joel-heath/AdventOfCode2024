ae8395eaeb034a20d581183e3f6621bb15f92b18
Joel Heath <joelheath@outlook.com>
2024-12-20 08:13:30 +0000

    day 20 p1 small optimisation (part 2 still incomprehensibly unattainable)

--- AdventOfCode2024/Day20.cs
+++ AdventOfCode2024/Day20.cs
@@ -34,6 +34,31 @@ public class Day20 : IDay
         return -1;
     }
 
+    // using the same idea as with a dijkstra's from A to C via B, you can do a dijkstra's from B, to A and C, and then sum the distances
+    private static int ShortestPath(Grid<char> map, Point start, Point via, Point end)
+    {
+        Queue<(Point, int)> queue = new([(via, 0)]);
+        HashSet<Point> visited = [via];
+        int distToStart = -1;
+        int distToEnd = -1;
+        while (queue.TryDequeue(out var data))
+        {
+            var (current, steps) = data;
+
+            if (distToEnd == -1 && current == end)
+                distToEnd = steps;
+            else if (distToStart == -1 && current == start)
+                distToStart = steps;
+
+            foreach (var next in map.Adjacents(current).Where(n => !visited.Contains(n) && map[n] != '#'))
+            {
+                visited.Add(next);
+                queue.Enqueue((next, steps + 1));
+            }
+        }
+        return distToStart + distToEnd;
+    }
+
     public string SolvePart1(string input)
     {
         var map = Grid<char>.FromString(input);
@@ -49,14 +74,13 @@ public class Day20 : IDay
             .Where(p => map.Adjacents(p).Count(n => map[n] != '#') >= 2))
         {    
             map[pos] = '.';
-            var len = ShortestPath(map, start, end);
-            if (len <= targetLength)
+            var len = ShortestPath(map, start, pos, end);
+            if (len > 0 && len <= targetLength)
                 count++;
             map[pos] = '#';
         }
 
         return $"{count}";
-
     }
 
     public string SolvePart2(string input)
