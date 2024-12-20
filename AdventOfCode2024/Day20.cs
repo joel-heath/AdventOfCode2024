1c9f26b7e5763018b645fa3edd64a9571129a626
Joel Heath <joelheath@outlook.com>
2024-12-20 07:54:10 +0000

    Day 20 part 1 solution

--- AdventOfCode2024/Day20.cs
+++ AdventOfCode2024/Day20.cs
@@ -1,4 +1,5 @@
 using AdventOfCode2024.Utilities;
+using System.Reflection.Metadata.Ecma335;
 
 namespace AdventOfCode2024;
 
@@ -7,18 +8,55 @@ public class Day20 : IDay
     public int Day => 20;
     public Dictionary<string, string> UnitTestsP1 => new()
     {
-        { "TestInput1", "ExpectedOutput1" },
-        { "TestInput2", "ExpectedOutput2" }
+        { "#####\r\n#S#E#\r\n#.#.#\r\n#...#\r\n#####", "2" },
+        { "###############\r\n#...#...#.....#\r\n#.#.#.#.#.###.#\r\n#S#...#.#.#...#\r\n#######.#.#.###\r\n#######.#.#...#\r\n#######.#.###.#\r\n###..E#...#...#\r\n###.#######.###\r\n#...###...#...#\r\n#.#####.#.###.#\r\n#.#...#.#.#...#\r\n#.#.#.#.#.#.###\r\n#...#...#...###\r\n###############", "44" },
     };
     public Dictionary<string, string> UnitTestsP2 => new()
     {
         { "TestInput1", "ExpectedOutput1" },
-        { "TestInput2", "ExpectedOutput2" }
     };
+
+    private static int ShortestPath(Grid<char> map, Point start, Point end)
+    {
+        Queue<(Point, int)> queue = new([(start, 0)]);
+        HashSet<Point> visited = [start];
+        while (queue.TryDequeue(out var data))
+        {
+            var (current, steps) = data;
+            if (current == end)
+                return steps;
+            foreach (var next in map.Adjacents(current).Where(n => !visited.Contains(n) && map[n] != '#'))
+            {
+                visited.Add(next);
+                queue.Enqueue((next, steps + 1));
+            }
+        }
+        return -1;
+    }
 
     public string SolvePart1(string input)
     {
-        return string.Empty;
+        var map = Grid<char>.FromString(input);
+        var start = map.AllPositions().First(p => map[p] == 'S');
+        var end = map.AllPositions().First(p => map[p] == 'E');
+        var normLength = ShortestPath(map, start, end);
+        var targetLength = normLength - (UnitTestsP1.ContainsKey(input) ? 2 : 100);
+
+        int count = 0;
+        foreach (var pos in map.AllPositions()
+            .Where(p => map[p] == '#')
+            .Where(p => start.MDistanceTo(p) + p.MDistanceTo(end) <= targetLength)
+            .Where(p => map.Adjacents(p).Count(n => map[n] != '#') >= 2))
+        {    
+            map[pos] = '.';
+            var len = ShortestPath(map, start, end);
+            if (len <= targetLength)
+                count++;
+            map[pos] = '#';
+        }
+
+        return $"{count}";
+
     }
 
     public string SolvePart2(string input)
