339fda597369d51761eb07642476ed95d9f1ed02
Joel Heath <joelheath@outlook.com>
2024-12-25 05:21:37 +0000

    Day 25 done yippee

--- AdventOfCode2024/Day25.cs
+++ AdventOfCode2024/Day25.cs
@@ -7,22 +7,47 @@ public class Day25 : IDay
     public int Day => 25;
     public Dictionary<string, string> UnitTestsP1 => new()
     {
-        { "TestInput1", "ExpectedOutput1" },
-        { "TestInput2", "ExpectedOutput2" }
+        { "#####\r\n.####\r\n.####\r\n.####\r\n.#.#.\r\n.#...\r\n.....\r\n\r\n#####\r\n##.##\r\n.#.##\r\n...##\r\n...#.\r\n...#.\r\n.....\r\n\r\n.....\r\n#....\r\n#....\r\n#...#\r\n#.#.#\r\n#.###\r\n#####\r\n\r\n.....\r\n.....\r\n#.#..\r\n###..\r\n###.#\r\n###.#\r\n#####\r\n\r\n.....\r\n.....\r\n.....\r\n#....\r\n#.#..\r\n#.#.#\r\n#####", "3" },
     };
     public Dictionary<string, string> UnitTestsP2 => new()
     {
-        { "TestInput1", "ExpectedOutput1" },
-        { "TestInput2", "ExpectedOutput2" }
+        { "Could you bring the chronicle Santa requested up to Santa?", "Deliver the chronicle." }
     };
 
     public string SolvePart1(string input)
     {
-        return string.Empty;
+        long summation = 0;
+
+        var schematics = input.Split(Environment.NewLine + Environment.NewLine);
+
+        var data = schematics
+            .Select(schem => schem.Split(Environment.NewLine))
+            .Select(rows => (rows, isKey: rows[0].Count(c => c == '#') == 0))
+            .Select(data => (data.rows
+                .Transpose()
+                .Select(column => column.Count(c => c == '#'))
+                .ToArray(), data.isKey))
+            .ToArray();
+
+        var fullKeyLength = schematics
+            .Select(schem => schem.Split(Environment.NewLine)
+                .Transpose()
+                .First().Length).First();
+
+        var keyLockFits = 0;
+        foreach (var (@lock, _) in data.Where(s => !s.isKey))
+        {
+            foreach (var (key, _) in data.Where(s => s.isKey))
+            {
+                if (@lock.Zip(key).All(t => t.First + t.Second <= fullKeyLength))
+                {
+                    keyLockFits++;
+                }
+            }
+        }
+
+        return $"{keyLockFits}";
     }
 
-    public string SolvePart2(string input)
-    {
-        return string.Empty;
-    }
+    public string SolvePart2(string input) => "Deliver the chronicle.";
 }
